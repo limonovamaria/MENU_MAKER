93ed8c9a0757eb033129500b14f5afa983fa7bfd
Лимонова Мария <Marusia.limonova@gmail.com>
2023-04-22 12:08:24 +0500

    Классы

---
+++ MenuMaker/MenuMaker/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuMaker
+{
+    internal class Person
+    {
+        int Age { get; set; }
+        int Weight { get; set; }
+        int Height { get; set; }
+        int Level { get; set; }
+        string Name { get; set; }
+        public Person(int age,int weight,int height,int level,string name)
+        {
+            Age = age;
+            Weight = weight;
+            Height = height;
+            Level = level;
+            Name = name;
+        }
+    }
+}
---
+++ MenuMaker/MenuMaker/Storage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuMaker
+{
+    internal class Storage:List<Product>
+    {
+        public Storage()
+        {
+
+        }
+        public void ADD(Product c)
+        {
+            this.Add(c);
+        }
+        public List<Product> MakeMenu(Person p)
+        {
+            return this;
+        }
+        public void Delete(Product c)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this[i].Name == c.Name)
+                {
+                    if (this[i].Weight > c.Weight)
+                    {
+                        this[i].Weight=this[i].Weight-c.Weight;
+                    }
+                    else
+                    {
+                        this.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+}
