5292d5305dc6ae9b4bf1349971c3a587774435ca
Yety-wishdom <satanpower4225@gmail.com>
2023-05-12 12:29:36 +0500

    Куча файлов сразу

---
+++ MyWebApp/Pages/Refreg.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages
+{
+    public class RefregModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
