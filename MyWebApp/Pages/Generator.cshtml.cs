4f131d96306ec3a589547bd7fb4e3d46f6faf28a
Yety-wishdom <satanpower4225@gmail.com>
2023-06-15 13:33:14 +0500

    Добавил функцию преобразования
    в JSON-файл из таблицы выбранных продуктов при заданной максимальной граммовке в localStorage

---
+++ MyWebApp/Pages/Generator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages
+{
+    public class GeneratorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
